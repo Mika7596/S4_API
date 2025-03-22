c1e4d467f6c6327d237bb9a21c4f5fec06f04325
Mika7596 <mika7596@gmail.com>
2025-03-22 12:21:23 +0100

    Complied changes to JS file.

--- dist/script.js
+++ dist/script.js
@@ -1,19 +1,2 @@
 "use strict";
-const divWeather = document.getElementById("divWeather");
-divWeather.innerHTML = "Holaaa";
-function getAJoke() {
-    fetch('https://icanhazdadjoke.com/').then(response => console.log(response));
-}
-const url = "https://pokeapi.co/api/v2/pokemon/pikachu/";
-async function getPokemon() {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data.weight);
-    }
-    catch (error) {
-        console.log("Error fetching pokemons: ", error);
-    }
-}
-getPokemon();
 //# sourceMappingURL=script.js.map
