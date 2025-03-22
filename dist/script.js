e1c0044a8f2c942c0d6df91b901655bcc665c0fd
Mika7596 <mika7596@gmail.com>
2025-03-22 13:12:39 +0100

    Added a function that fetches weather info from an API.

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,20 @@
 "use strict";
+const url = 'https://yahoo-weather5.p.rapidapi.com/weather?location=Barcelona&format=json&u=c';
+const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '9d6679a56bmshe7aac32a188b522p181bd4jsnb2ed9881b884',
+        'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com'
+    }
+};
+async function getWeather() {
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
 //# sourceMappingURL=script.js.map
---
+++ src/script.ts
@@ -0,0 +1,22 @@
+const url = 'https://yahoo-weather5.p.rapidapi.com/weather?location=Barcelona&format=json&u=c';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '9d6679a56bmshe7aac32a188b522p181bd4jsnb2ed9881b884',
+		'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com'
+	}
+};
+
+async function getWeather() {
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+// getWeather();
